0995379375aa7882ee0fa5d37d80cefec9a8c320
gamadosa <park980728@gmail.com>
2023-06-21 03:19:48 +0900

    commit

--- js/test.js
+++ js/test.js
@@ -11,9 +11,35 @@ window.addEventListener('scroll', function() {
   
 
 function scrollToTop() {
-    window.scrollTo({
-      top: 0,
-      behavior: 'smooth' 
-    });
-  }
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth' 
+  });
+}
+
+function buttonClick(btnNumber) {
+  //버튼을 눌렀을때 모든 clicked 클래스 제거
+  let clickedBtnArr = document.getElementsByClassName("clicked");  
+  Array.from(clickedBtnArr).forEach(function(x) {
+      x.classList.remove("clicked");
+  });
+  
+  //선택한 버튼 ckicked 추가
+  let clickedButton = document.getElementById("button" + btnNumber);
+  clickedButton.classList.add("clicked");
+
+
+  //버튼넘버에 맞는 내용보여주기 
+
+  let indexArr = document.getElementsByClassName("index");  
+  Array.from(indexArr).forEach(x => {
+    x.style.display = 'none';
+  });
+   
+  let index = document.getElementById("index"+btnNumber);
+  index.style.display = 'block';
+
+
+  
+}
 
